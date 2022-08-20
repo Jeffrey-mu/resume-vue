d4eaefac3c76d33b4fc6afa8bfdb79484359e54a
Jeffrey <1115378579@qq.com>
2022-08-20 17:03:44 +0800

    wip: 开发打印功能

--- src/components/resume/Operation.vue
+++ src/components/resume/Operation.vue
@@ -1,7 +1,19 @@
+<script setup lang="ts" name="operation">
+function print() {
+  window.print();
+}
+</script>
 <template>
-  <div w-40 flex="~ col" m-5>
+  <div w-40 flex="~ col" m-5 class="box_btn">
     <a-button mb-3 bg="#1890ff" c-white>进行配置</a-button>
     <a-button mb-3 bg="#1890ff" c-white>保存简历</a-button>
-    <a-button mb-3 bg="#1890ff" c-white>PDF下载</a-button>
+    <a-button mb-3 bg="#1890ff" c-white @click="print">PDF下载</a-button>
   </div>
 </template>
+<style>
+@media print {
+  .box_btn {
+    display: none !important;
+  }
+}
+</style>
