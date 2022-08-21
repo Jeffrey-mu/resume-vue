2db3dcf74d9ca54a2461dc806febdeb47548f461
Jeffrey <1115378579@qq.com>
2022-08-21 19:59:30 +0800

    feat：添加公共组件button

---
+++ src/components/Button.vue
@@ -0,0 +1,9 @@
+<template>
+<a-button mb-3 bg="#1890ff" c-white @click="emit('click')">
+  <slot></slot>
+</a-button>
+</template>
+<script setup lang="ts" name="button">
+const emit = defineEmits(["click"]);
+</script>
+
